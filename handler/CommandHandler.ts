e01d0d0490f7aa34dc39eb3eb0744d3ec0f8a648
Tsuchiya <amineprojet7@gmail.com>
2022-09-08 22:14:33 +0200

    chore(commands): logs everything

--- handler/CommandHandler.ts
+++ handler/CommandHandler.ts
@@ -1,9 +1,11 @@
 import { ChatUserstate, Client } from "tmi.js";
 import { Command } from "@/types";
+import {Loggers} from "@repositories/logger";
 
 export class CommandHandler {
     public readonly client: Client;
     commands: Command[];
+    public logger!: Loggers;
     constructor(client: Client) {
         this.client = client;
         this.commands = [];
@@ -16,15 +18,19 @@ export class CommandHandler {
         }
         const cmd = this.commands.find((cmd) => cmd.name === command);
         if (!cmd) {
+            await this.logger.warn(`* command "${command}" not found`);
             return;
         }
         if (cmd.permission === Permission.MOD && !tags.mod) {
+            await this.logger.warn(`* User ${tags.username} tried to use a mod command!`);
             return;
         }
         if (cmd.permission === Permission.SUBSCRIBER && !tags.subscriber) {
+            await this.logger.warn(`* User ${tags.username} tried to use a subscriber command!`);
             return;
         }
         if (cmd.permission === Permission.BROADCASTER && tags.username !== channel.slice(1)) {
+            await this.logger.warn(`* User ${tags.username} tried to use a broadcaster command!`);
             return;
         }
         cmd.execute(this.client, args, tags);
