e51053680736a5d9804725c78d94fabd45269efa
Amine <amineprojet7@gmail.com>
2022-09-09 15:43:30 +0200

    fix(logger): ok i promise this

--- repositories/logger.ts
+++ repositories/logger.ts
@@ -9,20 +9,20 @@ export class Loggers {
         // ...
     }
     public async log(message: string) {
-        this.logger.cyan(message);
+        await this.logger.cyan(message);
     }
     public async error(message: string) {
-        this.logger.error(message);
+        await this.logger.error(message);
     }
     public async warn(message: string) {
-        this.logger.warn(message);
+        await this.logger.warn(message);
     }
     public async info(message: string) {
-        this.logger.info(message);
+        await this.logger.info(message);
     }
     public async debug(message: string) {
         if(process.env.DEBUG) {
-            this.logger.warn(`DEBUG: ${message}`)
+            await this.logger.warn(`DEBUG: ${message}`)
         }
     }
 }
